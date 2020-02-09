b4978498579af0bc642834ef4e0d3cc86fdc0e02
liancheng <lfang42@wisc.edu>
2020-02-09 13:20:27 -0600

    simplify

--- test_square.py
+++ test_square.py
@@ -10,72 +10,29 @@ device = torch.device("cuda")
 
 # N is batch size; D_in is input dimension;
 # H is hidden dimension; D_out is output dimension.
-N, D_in, H, D_out = 4,4,4,4
+N, D_in, H, D_out = 4, 4, 4, 4
 
-# Create random Tensors to hold input and outputs.
-x = torch.randn(N, D_in, device=device, dtype=dtype)
-#construct orthogonal matrix
-q,r = torch.qr(x)
-x = torch.diag(torch.Tensor([0.75,1,2,3])).to(device)
-print("x:", x)
-
+# Create random Tensors to hold input and outputs, x is made to be diagonal matrix
+diag_x = torch.diag(torch.Tensor([0.75, 1, 2, 3])).to(device)
 y = torch.randn(N, D_out, device=device, dtype=dtype)
 
-# Create random Tensors for weights.
-#w1 = torch.randn(D_in, H, device=device, dtype=dtype, requires_grad=True)
+# Create random Tensors for weights. w1 is initialize as identity matrix
 w1 = torch.eye(4, dtype=dtype, device=device, requires_grad=True)
-#w1 = q.clone().detach().requires_grad_(True).to(device)
-#w1 = diag_weight.clone().detach().requires_grad_(True).to(device)
-print('w1:', w1)
 w2 = torch.randn(H, D_out, device=device, dtype=dtype, requires_grad=True)
-
-# #Method 2
-#         eig_dict_xtx = {}
-#         n_eigens_v = curr_core.shape[1]
-#         print('curr_core shape:', curr_core.shape)
-#         xtx = curr_core.t().mm(curr_core)
-#         ei_value, _ = torch.eig(xtx)
-#         #print('eigen-value:', ei_value)
-#
-#         # For symmetric pd matrix, SVD and eigen-decomp coincide, columns of u_xxt is eigen-vector of xxt, which is also left singular vector of x
-#         # columns of v_xtx is eigen-vector of xtx, which is also right singular vector of x
-#         with torch.no_grad():
-#             _, _, v_xtx = torch.svd(xtx)
-#             for i in range(n_eigens_v):
-#                 eig_dict_xtx.update({str(i): v_xtx[:, i][..., None]})
-#
-#         power_layer = power_iteration_once.apply
-#         for i in range(n_eigens_v):
-#             # columns of V
-#             eig_dict_xtx[str(i)] = power_layer(xtx, eig_dict_xtx[str(i)])
-#         V_column = []
-#         for i in eig_dict_xtx.keys():
-#             V_column.append(eig_dict_xtx[i])
-#         V = torch.cat(V_column, dim=1)
-#         #we solve U s.t UV' = curr_core, take transpose, VU' = curr_core'
-#         U_t, _ = torch.solve(curr_core.permute(1, 0), V)
-#         U = U_t.t()
-
 
 learning_rate = 1e-6
 for t in range(500):
 
-    #ip1 have eigen value 1 10 50 100
-    ip1 = (w1.mm(x)).mm(w1.t()).contiguous().view(4, -1)
-    eig_dict_xtx = {}
-    n_eigens_v = ip1.shape[1]
-    print("ip1 shape:", ip1.shape)
+    """ip1=w1*x*w1^T have eigen value 0.75,1,2,3"""
+    ip1 = (w1.mm(diag_x)).mm(w1.t())
 
-    #make sure its square matrix
-    assert(ip1.shape[0] == ip1.shape[1])
-    """ip1 = w1*x*w1^T, ip1^T*ip1 = w1*x*w1^T*w1*x*w1^T = w1*x*x*w1^T, so xtx will have eigenvalue/singular value [1,100,400,900], so is PD"""
+    """xtx = ip1^T*ip1, xtx initially have eigenvalue/singular value [0.75^2,1,4,9]"""
     xtx = ip1.t().mm(ip1)
     print('xtx:', xtx)
-    #pdb.set_trace()
 
+    eig_dict_xtx = {}
+    n_eigens_v = ip1.shape[1]
     with torch.no_grad():
-        #pdb.set_trace()
-        print("begin svd")
         print("cond number for input of svd:", LA.cond(xtx.cpu().data.numpy()))
         _, _, v_xtx = torch.svd(xtx)
         for i in range(n_eigens_v):
@@ -85,28 +42,25 @@ for t in range(500):
     for i in range(n_eigens_v):
         # columns of V
         eig_dict_xtx[str(i)] = power_layer(xtx, eig_dict_xtx[str(i)])
+
+    #Use eigen-vector as columns of V
     V_column = []
     for i in eig_dict_xtx.keys():
         V_column.append(eig_dict_xtx[i])
     V = torch.cat(V_column, dim=1)
 
-    #U_t, _ = torch.solve(ip1.permute(1, 0), V)
-    #print('torch.solve completed!')
-    #U = U_t.t()
-
     y_pred = V.mm(w2)
 
     # Compute and print loss
-    loss = (y_pred - y).pow(2).sum()
+    loss = (y_pred - y).pow(2).mean()
+    print('loss:', loss)
     if t % 100 == 99:
         print(t, loss.item())
 
     # Use autograd to compute the backward pass.
-    print("begin backward!")
     loss.backward()
 
     # Update weights using gradient descent
-    print("begin update!")
     with torch.no_grad():
         print('w1,2 grad:', w1.grad, w2.grad)
         w1 -= learning_rate * w1.grad
